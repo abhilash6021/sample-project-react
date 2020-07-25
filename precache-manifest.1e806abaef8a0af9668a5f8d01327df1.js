ae4922858e62682b6f83d8e3852e9878077ffdfc
abhilash soma <abhilashsoma@abhilashs-MacBook-Pro.local>
2020-07-25 14:27:06 -0500

    Updates

---
+++ precache-manifest.1e806abaef8a0af9668a5f8d01327df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c6ff5276cffe75c2a1876d62b640db",
+    "url": "/sample-project-react/index.html"
+  },
+  {
+    "revision": "0f6bc88a159ed421115a",
+    "url": "/sample-project-react/static/css/main.c88dbe49.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/sample-project-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-project-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6bc88a159ed421115a",
+    "url": "/sample-project-react/static/js/main.9ff4ac6c.chunk.js"
+  },
+  {
+    "revision": "367d07c7512cfdbf609d",
+    "url": "/sample-project-react/static/js/runtime-main.60097669.js"
+  }
+]);
